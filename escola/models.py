ee290e64c724df7d64e7dfd1719cd38e1b8b47c0
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2026-06-03 20:13:23 -0300

    Criação dos models "Materia" e "Aluno" e execução da migração dos dados.

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Materia(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome da Matéria')
+    descricao = models.TextField(
+        blank=True, null=True, verbose_name='Ementa/Descrição')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Matéria'
+        verbose_name_plural = 'Matérias'
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome do Aluno')
+    matricula = models.CharField(
+        max_length=20, unique=True, verbose_name='Matrícula')
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    email = models.EmailField(verbose_name='Email do Aluno')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'

0f178bf0f20bd1cf683f3be0e2b2d342a9478629
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2026-06-03 22:10:17 -0300

    Criando o Model de Turma, alterando o model aluno e registrando turma e aluno no admin.

--- escola/admin.py
+++ escola/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Materia, Aluno
+from .models import Materia, Aluno, Turma
 
 
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'descricao')  # Colunas que aparecerão na listagem
     search_fields = ('nome',)  # Campo de busca
+
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ano_letivo')  # Colunas que aparecerão na listagem
+    search_fields = ('nome',)  # Campo de busca
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'data_nascimento',
+                    'email')  # Colunas que aparecerão na listagem
+    search_fields = ('nome', 'matricula')  # Campo de busca
+    list_filter = ('data_nascimento',)  # Filtro lateral por data de nascimento
--- escola/models.py
+++ escola/models.py
@@ -14,15 +14,33 @@ class Materia(models.Model):
         verbose_name_plural = 'Matérias'
 
 
+class Turma(models.Model):
+    # Usamos CharField para nomes como "6º Ano A" ou "Maternal II"
+    nome = models.CharField(max_length=100, verbose_name='Nome da Turma')
+    # PositiveIntegerField garante que o ano não seja negativo (ex: 2026)
+    ano_letivo = models.PositiveIntegerField(verbose_name='Ano Letivo')
+
+    def __str__(self):
+        return f"{self.nome} ({self.ano_letivo})"
+
+    class Meta:
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
+
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome do Aluno')
     matricula = models.CharField(
         max_length=20, unique=True, verbose_name='Matrícula')
     data_nascimento = models.DateField(verbose_name='Data de Nascimento')
     email = models.EmailField(verbose_name='Email do Aluno')
+    # Relacionamento Muitos-para-Muitos: Um aluno pode estar em mais de uma
+    # turma, e uma turma obviamente tem muitos alunos.
+    turmas = models.ManyToManyField(
+        Turma, related_name='alunos', verbose_name='Turmas')
 
     def __str__(self):
-        return self.nome
+        return f'{self.matricula} - {self.nome}'
 
     class Meta:
         verbose_name = 'Aluno'

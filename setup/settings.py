e5f5f61c5ed365a462a7643edd80570452ce67fa
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2026-06-04 20:27:16 -0300

    FIX: Correção PEP8 no arquivo settings.py do projeto. (Tirando as mensagens de aviso de linhas que ultrapassam 79 colunas)

--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)4p!g@g7ig5v*_6dh1a$i-jd6jyl79bxo=4p7ke&)1*4(4im6a'
+SECRET_KEY = (
+    'django-insecure-)4p!g@g7ig5v*_6dh1a$i-jd6jyl79bxo=4p7ke&)1*4(4im6a'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,16 +90,23 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.MinimumLengthValidator',
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.CommonPasswordValidator',
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.NumericPasswordValidator',
+        ),
     },
 ]
 
